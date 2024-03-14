1ffe8afcdeaf0ecdd2422410a8319b2ccd3f3789
billybabis <billybabis@gmail.com>
2024-03-14 17:43:10 -0400

    display format

--- interface.py
+++ interface.py
@@ -11,7 +11,20 @@ def upload_zip():
         st.success("File uploaded successfully!")
         
 def input_main_query():
-    st.session_state["main_query_input"] = st.text_input("Enter your main query here:")
+    query_descr = ('In the following textbox, insert your master query which will be asked to ChatGPT '
+                   'for each of the "columns" listed in the table below. Please note that the keywords '
+                   '"{column_name}", "{column_description}", and "{excerpts}" will be replaced by the '
+                   'values found in the table below and the relevant text excerpts from each uploaded document. '
+                   'This query will be run iteratively for each document and for each data column. \n\n '
+                   'Example query: From the following text excerpts, list any references to “{column_name}” '
+                   'which we define as “{column_description}”. Only include direct quotation '
+                   'with the corresponding page number(s) (seen in square brackets at the end '
+                   'of each excerpt) with a brief explanation of the context of this quote within '
+                   'the text. It is very important not to hallucinate. '
+                   'Text: {excerpts} \n\n'
+                    'NOTE: Do not include any single quotation marks or apostraphes'
+                    )
+    st.session_state["main_query_input"] = st.text_input(query_descr)
 
 def input_email():
     st.session_state["email"] = st.text_input("Enter your email where you'd like to recieve the results:")
@@ -19,15 +32,15 @@ def input_email():
 def input_data_specs():
     if 'num_rows' not in st.session_state:
         st.session_state['num_rows'] = 1  # Starting with 1 row
-    def add_row():
-        st.session_state['num_rows'] += 1
-    st.button("Add Row", on_click=add_row)
     for i in range(st.session_state['num_rows']):
         col1, col2 = st.columns(2)
         with col1:
-            st.text_input(f"Column 1, Row {i+1}", key=f"col1_{i}")
+            st.text_input(f"Coluumn name", key=f"col1_{i}")
         with col2:
-            st.text_input(f"Column 2, Row {i+1}", key=f"col2_{i}")
+            st.text_input(f"Column description", key=f"col2_{i}")
+    def add_row():
+        st.session_state['num_rows'] += 1
+    st.button("Add Row", on_click=add_row)
     
 def process_table():
     column_specs =  {}

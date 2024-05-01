88e28e3808fe091d417e8c618d039db48a6c28fd
billybabis <billybabis@gmail.com>
2024-05-01 13:39:34 -0400

    small wording edits

--- interface.py
+++ interface.py
@@ -260,9 +260,11 @@ def about_tab():
 The GPT-Batch Policy Processor (beta) tool has been published on May 1st, 2024.
 \n
 **The GPT-Batch Policy Processor (beta) tool** \n
- Babis, William / Miquel Munoz-Cabre / Adis Dzebo (2024): GPT-Batch Policy Processor (beta). Stockholm Environment Institute (SEI).
+Babis, William / Munoz-Cabre, Miquel / Dzebo, Adis / Martelo, Camilo / Salzano, Cora / Torres Morales, Eileen / Arsadita, Ferosa (2024): GPT-Batch Policy Processor (beta). Stockholm Environment Institute (SEI).
 \n
 **Referring to GPT-Batch Policy Processor (beta) tool analysis** \n
- The Stockholm Environment Institute (SEI) hold the copyright of the GPT-Batch Policy Processor (beta) tool. It is licensed under Creative Commons and you are free to copy and redistribute material derived from the  GPT-Batch Policy Processor (beta) tool by following the guideline of the Creative Commons License. CC BY-ND (full attribution, no derivatives).
+ The Stockholm Environment Institute (SEI) hold the copyright of the GPT-Batch Policy Processor (beta) tool. It is 
+ licensed under Creative Commons and you are free to copy and redistribute material derived from the  GPT-Batch Policy 
+ Processor (beta) tool by following the guideline of the Creative Commons License. [CC BY-NC-NA](https://creativecommons.org/licenses/by-nc-sa/4.0/) (Attribution, NonCommercial, ShareAlike).
 """
     st.markdown(text)

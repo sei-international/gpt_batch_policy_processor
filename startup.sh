5e31144008d983ff4e02997ba66da0a039fce76b
Adis Dzebo <adis.dzebo@sei.org>
2025-08-27 14:43:23 +0200

    Remove unused startup.sh file

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Remove compressed output to prevent extraction to temp directory
-rm -f /home/site/wwwroot/output.tar.gz
-# Remove any existing manifest to force running from /home/site/wwwroot
-rm -f /home/site/wwwroot/oryx-manifest.toml
-# Start the application
-cd /home/site/wwwroot && python -m streamlit run main.py --server.port=8000 --server.address=0.0.0.0
